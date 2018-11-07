7f16c183ad05e57cf5a3367bebcb0c7d3f4e392a
kristof <christophe-dossantos@outlook.com>
2018-11-07 19:48:42 +0100

    Push Nuxt app to github

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/main.css'],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, { isDev }) {
+      if (isDev && process.client) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+
+  },
+  serverMiddleware: [
+    // API middleware
+    '~/api/index.js'
+  ]
+}
